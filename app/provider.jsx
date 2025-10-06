a144a86f48f17e7b561717eecb57c38265b13ca9
prk04 <magnetz1k@gmail.com>
2025-10-07 03:21:45 +0700

    first commit

---
+++ app/provider.jsx
@@ -0,0 +1,27 @@
+"use client"
+import { useUser } from '@clerk/nextjs';
+import axios from 'axios'
+import React, { useEffect } from 'react'
+
+function Provider({ children }) {
+
+  const {user}=useUser();
+
+  useEffect(()=>{
+      user&&CreateNewUser();
+  },[user])
+
+  const CreateNewUser=async()=>{
+    const result= await axios.post('/api/user',{
+            name:user?.fullName,
+            email:user?.primaryEmaillAdress?.emailAdress
+
+    });
+    console.log(result.data);
+  }
+  return (
+    <div> {children} </div>
+  )
+}
+
+export default Provider
