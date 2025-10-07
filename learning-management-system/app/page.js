6a57e01df7a806cc47cb69d8d7f3e54c6a7b9f4a
Nanon1707 <cutter7ven@gmail.com>
2025-10-07 18:52:11 +0700

    bug fix

--- learning-management-system/app/page.js
+++ learning-management-system/app/page.js
@@ -1,11 +1,9 @@
-
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
-      
       <UserButton />
     </div>
   );
